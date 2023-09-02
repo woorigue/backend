07e052692a67296e4b997314990e971cb9aa5217
정세욱 <tpdnrqkqh@gmail.com>
2023-09-03 01:19:17 +0900

    feat: province router

--- app/rest_api/api/__init__.py
+++ app/rest_api/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from app.rest_api.api.province import province_router
 from app.rest_api.api.user import user_router
 
 rest_router = APIRouter()
 rest_router.include_router(user_router)
+rest_router.include_router(province_router)
