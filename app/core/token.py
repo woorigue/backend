eecdb4809c1c50a55049e3f6fc2cdd6051321435
정세욱 <tpdnrqkqh@gmail.com>
2023-09-03 01:15:26 +0900

    feat: validate refresh token function and get current user

--- app/core/token.py
+++ app/core/token.py
@@ -1,7 +1,15 @@
 from datetime import datetime, timedelta
+from typing import Annotated
 
-from jose import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import APIKeyHeader
+from jose import ExpiredSignatureError, JWTError, jwt
 from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.core.deps import get_db
+from app.model.user import User
 
 SECRET_KEY = "JWTSECRETKEY"
 ALGORITHM = "HS256"
@@ -9,6 +17,7 @@ ACCESS_TOKEN_EXPIRE_DAYS = 1
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+bearer_header = APIKeyHeader(name="bearer", scheme_name="bearer")
 
 
 def verify_password(plain_password, hashed_password):
@@ -35,3 +44,52 @@ def create_refresh_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def get_current_user(
+    token: Annotated[str, Depends(bearer_header)], db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={"system_code": "EXPIRED_REFRESH_TOKEN"},
+        )
+    except JWTError:
+        raise credentials_exception
+    user = db.scalar(select(User).where(User.email == username))
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+def validate_refresh_token(
+    token: Annotated[str, Depends(bearer_header)], db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token.refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={"system_code": "EXPIRED_REFRESH_TOKEN"},
+        )
+    except JWTError:
+        raise credentials_exception
