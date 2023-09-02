7219f90ef0db5adef85d677d7cd9615a086c728d
정세욱 <tpdnrqkqh@gmail.com>
2023-09-03 01:17:42 +0900

    feat: get access token using refresh token api

--- app/rest_api/api/user.py
+++ app/rest_api/api/user.py
@@ -1,9 +1,17 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.core.deps import get_db
-from app.core.token import create_access_token, create_refresh_token, verify_password
+from app.core.token import (
+    create_access_token,
+    create_refresh_token,
+    get_current_user,
+    validate_refresh_token,
+    verify_password,
+)
 from app.model.user import User
 from app.rest_api.controller.email import email_controller as email_con
 from app.rest_api.controller.user import user_controller as con
@@ -13,6 +21,7 @@ from app.rest_api.schema.email import (
     EmailPasswordResetSchema,
     EmailVerifySchema,
 )
+from app.rest_api.schema.token import RefreshTokenSchema
 from app.rest_api.schema.user import (
     EmailLoginSchema,
     EmailRegisterSchema,
@@ -72,12 +81,24 @@ def email_request_password_reset(
     user_data: EmailPasswordResetSchema, db: Session = Depends(get_db)
 ):
     email_con.send_verify_code_for_reset_password(db, user_data)
-
     return {"success": True}
 
 
 @user_router.post("/password/reset")
 def user_reset_password(user_data: ResetPasswordSchema, db: Session = Depends(get_db)):
     con.reset_password(db, user_data)
-
     return {"success": True}
+
+
+@user_router.post("/token/refresh")
+def get_access_token_using_refresh_token(
+    user_data: RefreshTokenSchema,
+    token: Annotated[str, Depends(get_current_user)],
+    db: Session = Depends(get_db),
+):
+    validate_refresh_token(user_data, db)
+
+    access_token = create_access_token(data={"sub": token.email})
+    refresh_token = create_refresh_token(data={"sub": token.email})
+
+    return {"access_token": access_token, "refresh_token": refresh_token}
