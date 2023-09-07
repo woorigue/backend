6387bae2f1c61e138271982af067e1041ed13750
정세욱 <tpdnrqkqh@gmail.com>
2023-09-08 00:32:47 +0900

    feat: join position table

--- app/model/position.py
+++ app/model/position.py
@@ -1,5 +1,5 @@
-from fastapi import HTTPException, status
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.db.session import Base
 
@@ -9,3 +9,16 @@ class Position(Base):
 
     seq = Column(Integer, primary_key=True, comment="시퀀스")
     name = Column(String(12), unique=True, comment="이름")
+
+    join_position = relationship("JoinPosition", back_populates="position")
+
+
+class JoinPosition(Base):
+    __tablename__ = "join_position"
+
+    seq = Column(Integer, primary_key=True, comment="시퀀스")
+    position_seq = Column(Integer, ForeignKey("position.seq"))
+    profile_seq = Column(Integer, ForeignKey("profile.seq"))
+
+    position = relationship("Position", back_populates="join_position")
+    profile = relationship("Profile", back_populates="join_profile")
