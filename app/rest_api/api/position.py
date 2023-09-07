ed3c1c8fdf0231c5a1d8b5601862ceb4fca310c6
정세욱 <tpdnrqkqh@gmail.com>
2023-09-08 00:34:17 +0900

    feat: response model schema

--- app/rest_api/api/position.py
+++ app/rest_api/api/position.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import Session
 
 from app.core.deps import get_db
 from app.model.position import Position
+from app.rest_api.schema.position import PositionSchema
 
 position_router = APIRouter(tags=["position"], prefix="/position")
 
 
-@position_router.get("")
+@position_router.get("", response_model=PositionSchema)
 def get_positions(db: Session = Depends(get_db)):
     positions = db.query(Position).all()
     return positions
