d6b1f8355022311b34bbd4cacc104038355eaa83
sewookjeong <tpdnrqkqh@naver.com>
2023-10-16 00:09:29 +0900

    feat: img url upload function of profile

---
+++ app/rest_api/controller/file.py
@@ -0,0 +1,19 @@
+import boto3
+from io import BytesIO
+
+
+class FileController:
+    def __init__(self):
+        self.s3_client = boto3.client("s3")
+
+    def upload_uesr_profile_img(self, file, user, file_name, db):
+        url = f"profile/{user.email}/{file_name}"
+        content = BytesIO(file)
+        self.s3_client.upload_fileobj(content, "our-league", url)
+        profile = user.profile[0]
+        profile.img = "drl2968ia795g.cloudfront.net/" + url
+        db.commit()
+        db.flush()
+
+
+file_controller = FileController()
