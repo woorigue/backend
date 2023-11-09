54ee26c71a2d733d0a2ce58247b56ab43ea4e9b1
jin park <jinpark0625@gmail.com>
2023-11-08 20:03:29 -0500

    feat: Add banner model

---
+++ app/db/alembic/versions/71c407fe216c_banner_table.py
@@ -0,0 +1,30 @@
+"""banner table
+
+Revision ID: 71c407fe216c
+Revises: eeed23f6b95d
+Create Date: 2023-11-05 10:03:28.932207
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '71c407fe216c'
+down_revision: Union[str, None] = 'eeed23f6b95d'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "banner",
+        sa.Column("id", sa.Integer, primary_key=True, comment="아이디"),
+        sa.Column("url", sa.String(255), nullable=False, comment="주소"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('banner')
