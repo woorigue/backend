9edfa8f965a75a628853e5071e63a6221ba5e57f
jin park <jinpark0625@gmail.com>
2023-11-10 20:31:36 -0500

    feat: Add file upload

--- app/rest_api/api/banner.py
+++ app/rest_api/api/banner.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
 from sqlalchemy.orm import Session
 
 from app.core.deps import get_db
 from app.model.banner import Banner
+
+from app.rest_api.controller.file import file_controller as file_con
 
 from app.rest_api.schema.banner import (BannerRegisterSchema, BannerDeleteSchema)
 
@@ -14,10 +16,9 @@ def get_banners(db: Session = Depends(get_db)):
     return banners
     
 @banner_router.post("")
-def add_banners(user_data:BannerRegisterSchema, db: Session = Depends(get_db)):
-    new_banner = Banner(url=user_data.url)
-    db.add(new_banner)
-    db.commit()
+def add_banners(banner_img:UploadFile, db: Session = Depends(get_db)):
+    banner_content = await banner_img.read()
+    file_con.upload_banner_img(banner_content, banner_img.filename, db)
     return {"message": "배너가 성공적으로 등록되었습니다.", "banner": new_banner.url}
 
 @banner_router.delete("/{banner_id}")
