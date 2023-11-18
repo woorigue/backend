62e19c0f6059e408102f6d46dcb85066c0dc441b
sewookjeong <tpdnrqkqh@naver.com>
2023-11-18 23:13:18 +0900

    feat: session middleware

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.blueprints import register_router
 from app.core.errorhanlder import register_exception_handler
+from starlette.middleware.sessions import SessionMiddleware
 
 
 def create_application() -> FastAPI:
@@ -10,12 +11,14 @@ def create_application() -> FastAPI:
         title="우리들만의 리그 API",
         docs_url="/swagger",
     )
+    application.add_middleware(SessionMiddleware, secret_key="testscret")
     register_router(application)
     register_exception_handler(application)
     return application
 
 
 app = create_application()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
