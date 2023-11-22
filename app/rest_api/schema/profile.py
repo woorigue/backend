3f489529de8a0c263a7a33c942ed019b9eb3528f
m23jung <m23jung@gmail.com>
2023-11-21 19:32:56 -0500

    fixed get /user/me return error

--- app/rest_api/schema/profile.py
+++ app/rest_api/schema/profile.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 from pydantic import BaseModel, Field, StrictInt, StrictStr
 
@@ -12,6 +12,6 @@ class UpdateProfileSchema(BaseModel):
 
 class GetProfileSchema(BaseModel):
     nickname: StrictStr = Field(title="닉네임", default=None)
-    img: str = Field(title="이미지 URL", default=None)
+    img: Union[str, None] = Field(title="이미지 URL", default=None)
     sex: str = Field(title="성별", default="M")
     join_profile: List[JoinPositionSchema] = []
