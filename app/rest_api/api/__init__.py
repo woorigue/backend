a011fee015e8aa1f84a06052d68195745ef3f926
m23jung <m23jung@gmail.com>
2023-11-23 00:40:28 -0500

    Revert "created club model and api"
    This reverts commit d8055310d68be6582f7a82105fdbf0e6401ef835.

--- app/rest_api/api/__init__.py
+++ app/rest_api/api/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.rest_api.api.position import position_router
 from app.rest_api.api.province import province_router
 from app.rest_api.api.user import user_router
+<<<<<<< HEAD
 
 # Match
 from app.rest_api.api.match.match import match_router
@@ -12,13 +13,19 @@ from app.rest_api.api.guest.guest import guest_router
 from app.rest_api.api.notification.faq import faq_router
 # Etc(banner...)
 from app.rest_api.api.banner.banner import banner_router
+=======
+from app.rest_api.api.banner import banner_router
+>>>>>>> 3cb8454 (Revert "created club model and api")
 
 rest_router = APIRouter()
 rest_router.include_router(user_router)
 rest_router.include_router(province_router)
 rest_router.include_router(position_router)
 rest_router.include_router(banner_router)
+<<<<<<< HEAD
 rest_router.include_router(faq_router)
 rest_router.include_router(match_router)
 rest_router.include_router(guest_router)
 
+=======
+>>>>>>> 3cb8454 (Revert "created club model and api")
