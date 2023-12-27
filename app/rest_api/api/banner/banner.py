edadf849d1ad9650e5436016cacf6a01a784f837
jin park <jinpark0625@gmail.com>
2023-12-28 00:54:50 +0900

    feat: Refactor banner API and add PATCH method

---
+++ app/rest_api/api/banner/banner.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
+from sqlalchemy.orm import Session
+
+from app.core.deps import get_db
+from app.model.banner import Banner
+
+from app.rest_api.controller.file import file_controller as file_con
+
+from app.rest_api.schema.banner.banner import (BannerRegisterSchema, BannerDeleteSchema)
+
+from app.helper.exception import(
+    BannerNotFoundException
+)
+
+
+banner_router = APIRouter(tags=["banner"], prefix="/banner")
+
+@banner_router.get("")
+def list_banners(db: Session = Depends(get_db)):
+    banners = db.query(Banner).order_by(Banner.create_date.desc()).all()
+    return banners
+    
+@banner_router.get("/{banner_id}")
+def get_banner(banner_id:int, db: Session = Depends(get_db)):
+    banner = db.query(Banner).filter(Banner.seq == banner_id).first()
+    if banner is None:
+        raise BannerNotFoundException
+    return banner    
+    
+@banner_router.post("")
+async def add_banner(banner_img:UploadFile, db: Session = Depends(get_db)):
+    banner_content = await banner_img.read()
+    file_con.upload_banner_img(banner_content, banner_img.filename, db)
+    return {"success": True}
+    
+@banner_router.patch("/{banner_id}")
+async def edit_banner(banner_id:int, banner_img:UploadFile, db: Session = Depends(get_db)):
+    
+    banner = db.query(Banner).filter(Banner.seq == banner_id).first()
+    if not banner:
+        raise BannerNotFoundException
+    
+    banner_content = await banner_img.read()
+    new_url = file_con.edit_banner_img(banner_content, banner_img.filename)
+    
+    banner.url = new_url
+    
+    db.commit()
+    return {"success": True}
+
+
+@banner_router.delete("/{banner_id}")
+def delete_banner(banner_id:int, db: Session = Depends(get_db)):
+    banner = db.query(Banner).filter(Banner.seq == banner_id).first()
+    if not banner:
+        raise BannerNotFoundException
+        
+    db.delete(banner)
+    db.commit()
+    return {"message": "배너가 성공적으로 삭제되었습니다."}
