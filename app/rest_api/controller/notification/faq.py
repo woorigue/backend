57897699cc3372b49c837d540564cbc42159e38d
jin park <jinpark0625@gmail.com>
2023-12-28 01:14:36 +0900

    feat: Add faq controller

---
+++ app/rest_api/controller/notification/faq.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+
+from app.model.faq import Faq
+from app.rest_api.schema.notification.faq import FaqCreateSchema,FaqEditSchema
+
+class FaqController:
+    def create_faq(self, faq_data: FaqCreateSchema, db:Session)-> None:
+        Faq.create(faq_data, db)
+        
+    def edit_faq(self, faq_id:int, faq_data: FaqEditSchema, db:Session)-> None:
+        Faq.edit(faq_id, faq_data, db)
+        
+faq_controller = FaqController()
