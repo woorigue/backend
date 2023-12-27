d1f71a77d18d5025c2840667bee55eed5c7f2a99
jin park <jinpark0625@gmail.com>
2023-12-28 01:15:49 +0900

    feat: Add faq schema

---
+++ app/rest_api/schema/notification/faq.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+class FaqDeleteSchema(BaseModel):
+    id: int = Field(title="아이디")
+
+class FaqCreateSchema(BaseModel):
+    title: str = Field(title="제목")
+    body: str = Field(title="본문")
+    
+class FaqGetSchema(BaseModel):
+    title: str = Field(title="제목")
+    body: str = Field(title="본문")
+    
+class FaqEditSchema(BaseModel):
+    title: Optional[str] = None
+    body: Optional[str] = None
