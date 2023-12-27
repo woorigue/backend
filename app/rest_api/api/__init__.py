8bc4b9a4efbbbaedd18536e15abd9b3dff48cea1
jin park <jinpark0625@gmail.com>
2023-12-28 01:39:38 +0900

    feat: Add FAQ, Match, and Guest routers to API

--- app/rest_api/api/__init__.py
+++ app/rest_api/api/__init__.py
@@ -3,10 +3,22 @@ from fastapi import APIRouter
 from app.rest_api.api.position import position_router
 from app.rest_api.api.province import province_router
 from app.rest_api.api.user import user_router
-from app.rest_api.api.banner import banner_router
+
+# Match
+from app.rest_api.api.match.match import match_router
+# Guest
+from app.rest_api.api.guest.guest import guest_router
+# Notfication
+from app.rest_api.api.notification.faq import faq_router
+# Etc(banner...)
+from app.rest_api.api.banner.banner import banner_router
 
 rest_router = APIRouter()
 rest_router.include_router(user_router)
 rest_router.include_router(province_router)
 rest_router.include_router(position_router)
 rest_router.include_router(banner_router)
+rest_router.include_router(faq_router)
+rest_router.include_router(match_router)
+rest_router.include_router(guest_router)
+
