264738b763a25be75d174168baa816e948b19e1c
seuk.jeong <seuk.jeong@paywatchglobal.com>
2024-01-04 01:09:02 +0900

    feature: rabbitmq

--- app/rest_api/schema/banner/banner.py
+++ app/rest_api/schema/banner/banner.py
@@ -2,8 +2,14 @@ from typing import List
 
 from pydantic import BaseModel, Field
 
+
 class BannerRegisterSchema(BaseModel):
     url: str = Field(title="주소")
-    
+
+
 class BannerDeleteSchema(BaseModel):
+<<<<<<< HEAD:app/rest_api/schema/banner/banner.py
     seq: int = Field(title="아이디")
+=======
+    id: int = Field(title="아이디")
+>>>>>>> 93a53a9 (feature: rabbitmq):app/rest_api/schema/banner.py
