a88ef16276245db4cdef0f930af08a60d3eafc6a
seuk.jeong <seuk.jeong@paywatchglobal.com>
2024-02-26 00:14:32 +0900

    feature: basemodel interface model

--- app/core/model.py
+++ app/core/model.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, DateTime, Integer
 from datetime import datetime
 
 
 class BaseModel:
+    seq = Column(Integer, primary_key=True, autoincrement=True, comment="시퀀스")
     created_at = Column(DateTime, default=datetime.utcnow(), comment="생성 시간")
     updated_at = Column(
         DateTime,
