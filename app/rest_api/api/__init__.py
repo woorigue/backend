e4802f4085140deacf2c9b211f89ce1bb9319405
seuk.jeong <seuk.jeong@paywatchglobal.com>
2024-04-02 23:10:53 +0900

    refactor: apply isort & black to router main file

--- app/rest_api/api/__init__.py
+++ app/rest_api/api/__init__.py
@@ -1,18 +1,17 @@
 from fastapi import APIRouter
 
+from app.rest_api.api.banner import banner_router
+from app.rest_api.api.chat import chat_router
+from app.rest_api.api.club.club import club_router
+from app.rest_api.api.club.clubPosting import clubPosting_router
+from app.rest_api.api.guest.guest import guest_router
+from app.rest_api.api.match.match import match_router
+from app.rest_api.api.memberPosting import memberPosting_router
+from app.rest_api.api.notification.faq import faq_router
+from app.rest_api.api.poll import poll_router
 from app.rest_api.api.position import position_router
 from app.rest_api.api.province import province_router
 from app.rest_api.api.user import user_router
-from app.rest_api.api.match.match import match_router
-from app.rest_api.api.guest.guest import guest_router
-from app.rest_api.api.notification.faq import faq_router
-from app.rest_api.api.banner import banner_router
-from app.rest_api.api.club.club import club_router
-from app.rest_api.api.club.clubPosting import clubPosting_router
-from app.rest_api.api.chat import chat_router
-from app.rest_api.api.poll import poll_router
-from app.rest_api.api.memberPosting import memberPosting_router
-
 
 rest_router = APIRouter()
 rest_router.include_router(user_router)
