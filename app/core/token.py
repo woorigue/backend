7523757f2114f89abbb35afd62eeef1996297eea
seuk.jeong <seuk.jeong@paywatchglobal.com>
2024-09-12 23:08:06 +0900

    Revert "fix: create token"
    This reverts commit 1c6e1a1b4eef0c93f4a0991d94173bf69996832e.

--- app/core/token.py
+++ app/core/token.py
@@ -11,10 +11,10 @@ from sqlalchemy.orm import Session
 from app.core.deps import get_db
 from app.model.user import User
 
-SECRET_KEY = "NvbeIjVAT/MW8ihG5pWzb03pzI3PlSCRFJE8B0IzyUAlIzxXiJinD8ywbjQEfOilhkD+fARoW/SlWiU5LrFncw=="
+SECRET_KEY = "JWTSECRETKEY"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_DAYS = 365
-REFRESH_TOKEN_EXPIRE_DAYS = 365
+ACCESS_TOKEN_EXPIRE_DAYS = 1
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 bearer_header = APIKeyHeader(name="Authorization")
@@ -29,7 +29,7 @@ def create_access_token(data: dict):
 
     to_encode = data.copy()
     expire = datetime.now() + access_token_expires
-    to_encode.update({"exp": expire, "role": "authenticated", "aud": "authenticated"})
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
@@ -40,7 +40,7 @@ def create_refresh_token(data: dict):
 
     to_encode = data.copy()
     expire = datetime.now() + refresh_token_expires
-    to_encode.update({"exp": expire, "role": "authenticated", "aud": "authenticated"})
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
