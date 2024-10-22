97317d526fc4ce881ee9c1000b78563a87ee8392
seuk.jeong <seuk.jeong@paywatchglobal.com>
2024-10-22 23:46:20 +0900

    fix: default now date

--- app/core/model.py
+++ app/core/model.py
@@ -6,10 +6,10 @@ from app.db.session import Base
 class TimestampedModel(Base):
     __abstract__ = True  # 추상화
 
-    created_at = Column(DateTime, default=datetime.utcnow(), comment="생성 시간")
+    created_at = Column(DateTime, default=datetime.utcnow, comment="생성 시간")
     updated_at = Column(
         DateTime,
-        default=datetime.utcnow(),
-        onupdate=datetime.utcnow(),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
         comment="업데이트 시간",
     )
